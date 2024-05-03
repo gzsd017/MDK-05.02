36bec93a10058d718231b08ccc49e543b74cbb68
Zhidkova Svetlana <148215526+gzsd017@users.noreply.github.com>
2024-05-03 09:48:37 +0300

    Add files via upload

---
+++ 7 дкр код/dragon1.pas
@@ -0,0 +1,11 @@
+﻿program Dragon1;
+
+uses GraphABC, dragon_fractal, dragon_input_handler;
+
+begin
+  LockDrawing;
+  DrawDragonFractal(200, 140, 0, -4);
+  redraw;
+  
+  onKeyDown += HandleDragonInput; 
+end.
